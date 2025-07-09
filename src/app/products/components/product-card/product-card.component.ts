b216e3e5b26830323945834c78f8ca008102d810
GAMBSTER <gamboa040200@gmail.com>
2025-07-09 10:00:10 -0600

    feat: enhance ProductCardComponent to dynamically display product details and update HomePageComponent to render product cards from a list

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Product } from '@products/interfaces/products.interface';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './product-card.component.html',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  baseUrl = baseUrl;
+  product = input.required<Product>();
+  imgUrl = computed(() => {
+    return `${baseUrl}/files/product/${this.product().images[0]}`;
+  });
+}
