203bd2c70636fd6c335d3155509043f3c339cc04
GAMBSTER <gamboa040200@gmail.com>
2025-07-08 22:53:40 -0600

    feat: update ProductsService to accept options for product fetching and add environment configuration for development

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -13,7 +13,7 @@ export class HomePageComponent {
   productsResource = rxResource({
     request: () => ({}),
     loader: ({ request }) => {
-      return this.productsService.getProducts();
+      return this.productsService.getProducts({});
     },
   });
 }
